e0a405d2640a86340eb5beab169e5c4f00cb1997
Jnesspace <jacobn@spacelift.io>
2025-07-09 16:41:09 -0400

    test

--- stacks/admin/main.tf
+++ stacks/admin/main.tf
@@ -130,35 +130,3 @@ module "stack_ansible" {
     }
   }
 }
-
-resource "spacelift_stack" "kubernetes" {
-  name     = "Tofusible - Kubernetes"
-  space_id = var.resource_space_id
-
-  repository = "Quick-Cluster"
-  branch     = "main"
-
-  project_root = "stacks/kubernetes"
-
-  kubernetes {
-    kubectl_version = "1.33.2"
-  }
-
-  labels = ["tofusible", "kubernetes"]
-
-  autodeploy = true
-  enable_well_known_secret_masking = true
-  github_action_deploy = false
-}
-
-resource "spacelift_stack_dependency" "kubernetes_depends_on_ansible" {
-  stack_id            = spacelift_stack.kubernetes.id
-  depends_on_stack_id = module.stack_ansible.id
-}
-
-resource "spacelift_stack_dependency_reference" "kubernetes_kubeconfig" {
-  stack_dependency_id = spacelift_stack_dependency.kubernetes_depends_on_ansible.id
-  output_name         = "kubeconfig_content"
-  input_name          = "KUBE_CONFIG_FILE_CONTENT"
-  trigger_always      = true
-}
